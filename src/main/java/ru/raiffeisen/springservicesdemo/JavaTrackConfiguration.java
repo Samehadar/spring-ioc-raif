b0c7d6803edc2aef1a3eeb803ce4fe31bfec1451
LYUTAREVICH Vitaly <Vitaly.LYUTAREVICH@raiffeisen.ru>
2019-08-20 05:02:39 +0600

    Show creating bean using Java Configuration;

---
+++ src/main/java/ru/raiffeisen/springservicesdemo/JavaTrackConfiguration.java
@@ -0,0 +1,14 @@
+package ru.raiffeisen.springservicesdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaTrackConfiguration {
+
+    @Bean
+    public RapTrack getTrack() {
+        return new RapTrack("Трюк", "Domino");
+    }
+
+}
--- src/main/java/ru/raiffeisen/springservicesdemo/SpringServicesDemoApplication.java
+++ src/main/java/ru/raiffeisen/springservicesdemo/SpringServicesDemoApplication.java
@@ -8,13 +8,13 @@ import ru.raiffeisen.springservicesdemo.service.MusicService;
 
 
 @SpringBootApplication
-@ImportResource("classpath:tracks-configuration.xml")
+//@ImportResource("classpath:tracks-configuration.xml")
 public class SpringServicesDemoApplication {
 
 	public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(SpringServicesDemoApplication.class, args);
 
-        Track rapTrack = (Track) ctx.getBean("rapTrack");
+        Track rapTrack = (Track) ctx.getBean("getTrack");
         Track jazzTrack = new JazzTrack("Hymn for the Travelling Souls", "Кетиль Бьёрнстад");
         MusicService musicService = new MusicService(rapTrack);
 
