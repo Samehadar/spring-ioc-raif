0b78f664f7a13b1ecb7b57282eb2d1cc09c63e39
LYUTAREVICH Vitaly <Vitaly.LYUTAREVICH@raiffeisen.ru>
2019-08-20 05:00:19 +0600

    Show creating bean using XML;

--- src/main/java/ru/raiffeisen/springservicesdemo/SpringServicesDemoApplication.java
+++ src/main/java/ru/raiffeisen/springservicesdemo/SpringServicesDemoApplication.java
@@ -2,17 +2,21 @@ package ru.raiffeisen.springservicesdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import ru.raiffeisen.springservicesdemo.service.MusicService;
 
 
 @SpringBootApplication
+@ImportResource("classpath:tracks-configuration.xml")
 public class SpringServicesDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringServicesDemoApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringServicesDemoApplication.class, args);
 
-		Track track = new JazzTrack("Hymn for the Travelling Souls", "Кетиль Бьёрнстад");
-        MusicService musicService = new MusicService(track);
+        Track rapTrack = (Track) ctx.getBean("rapTrack");
+        Track jazzTrack = new JazzTrack("Hymn for the Travelling Souls", "Кетиль Бьёрнстад");
+        MusicService musicService = new MusicService(rapTrack);
 
         System.out.println(musicService.play());
     }
